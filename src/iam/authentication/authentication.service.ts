4cd08aac198bf60dea51e0dddcfbe5e835689b67
ysherqawi <ysherqawi@gmail.com>
2023-11-14 16:35:32 +0300

    automatic reuse detection

--- src/iam/authentication/authentication.service.ts
+++ src/iam/authentication/authentication.service.ts
@@ -15,7 +15,10 @@ import jwtConfig from '../config/jwt.config';
 import { ConfigType } from '@nestjs/config';
 import { ActiveUserData } from '../interfaces/active-user-data.interface';
 import { RefreshTokenDto } from './dto/refresh-token.dto';
-import { RefreshTokenIdsStorage } from './refresh-token-ids.storage';
+import {
+  InvalidatedRefreshTokenError,
+  RefreshTokenIdsStorage,
+} from './refresh-token-ids.storage';
 import { randomUUID } from 'crypto';
 
 @Injectable()
@@ -101,6 +104,10 @@ export class AuthenticationService {
       }
       return this.generateTokens(user);
     } catch (err) {
+      if (err instanceof InvalidatedRefreshTokenError) {
+        // Take action: notify user that his refresh token might have been stolen?
+        throw new UnauthorizedException('Access denied');
+      }
       throw new UnauthorizedException();
     }
   }
--- src/iam/authentication/refresh-token-ids.storage.ts
+++ src/iam/authentication/refresh-token-ids.storage.ts
@@ -5,6 +5,8 @@ import {
 } from '@nestjs/common';
 import Redis from 'ioredis';
 
+export class InvalidatedRefreshTokenError extends Error {}
+
 @Injectable()
 export class RefreshTokenIdsStorage
   implements OnApplicationBootstrap, OnApplicationShutdown
@@ -26,6 +28,9 @@ export class RefreshTokenIdsStorage
   }
   async validate(userId: number, tokenId: string): Promise<boolean> {
     const storedId = await this.redisClient.get(this.getKey(userId));
+    if (storedId !== tokenId) {
+      throw new InvalidatedRefreshTokenError();
+    }
     return storedId === tokenId;
   }
   async invalidate(userId: number): Promise<void> {
